40e605da2bdc8a48b48bf7ff20c662a1c577200b
Adilet Akamtov <adiletakmatovofficial@gmail.com>
2022-07-21 04:02:54 +0600

    add NewChatRoom and link Chatroom

---
+++ src/components/NewChatRoom.vue
@@ -0,0 +1,57 @@
+<template>
+  <form action="">
+    <textarea @keypress.prevent.enter="handleSubmit" 
+    placeholder="Type a message and hit enter to send"
+    v-model="message"
+    ></textarea>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+import { getUser } from "../composables/getUser"
+export default {
+  setup(){
+    const message = ref("");
+    const { user } = getUser();
+
+    const handleSubmit = () => {
+      console.log(message.value);
+    }
+
+    return { message,handleSubmit };
+  },
+
+}
+</script>
+
+<style scoped>
+  form {
+    margin: 10px;
+  }
+  textarea{
+    width: 100%;
+    margin-left: 10px;
+    margin-bottom: 6px;
+    box-sizing: border-box;
+    border: 0;
+    border-radius: 20px;
+    font-family: inherit;
+    outline: none;
+    resize: vertical;
+    position: relative;
+  }
+  textarea::placeholder{
+    position: absolute;
+    top:5px;
+    left: 5px;
+  }
+
+  .error{
+    text-align: center;
+    color: #ff2a58;
+    font-size: 12px;
+    padding: 10px 0;
+  }
+</style>>
+
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="container">
     <Navbar />
+    <NewChatRoom />
   </div>
 
 </template>
 
 <script>
 import Navbar from "../components/Navbar.vue"
+import NewChatRoom from "../components/NewChatRoom.vue"
 export default {
-  components: { Navbar },
+  components: { Navbar , NewChatRoom },
 }
 </script>
 
